fd9400f25b5061842f0b4555b86e0d9d63294502
amandayaworsky <amandayaworsky@gmail.com>
2026-02-05 17:45:54 -0500

    Create minamal_structure.py

---
+++ minamal_structure.py
@@ -0,0 +1,11 @@
+#Your files may include:
+
+#pass statements
+
+#Placeholder return values (e.g., return None)
+
+#No real computation
+
+#The emphasis is on thinking and explaining, not coding.
+
+ 
