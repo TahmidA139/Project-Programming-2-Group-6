fc38af812d6c35c6d191c77a3a4617fa784f562e
Nicole Decocker <edecocke@charlotte.edu>
2026-04-29 21:20:26 -0400

    fixing from statments __init__.py

--- src/orf_finder_lib/__init__.py
+++ src/orf_finder_lib/__init__.py
@@ -1,1 +1,6 @@
+"""ORF detection: frame scanning and ORF finding."""
 
+from src.orf_finder_lib.orf_finder import find_orfs, CSV_FIELDNAMES
+from src.orf_finder_lib.frame_scanner import scan_frame, extract_orf_sequence
+
+__all__ = ["find_orfs", "CSV_FIELDNAMES", "scan_frame", "extract_orf_sequence"]
